ace91e2368a794bc1b1388566a7a0cc3abe9b240
h3nr1 <65588683+yvnghenri@users.noreply.github.com>
2022-04-05 19:13:55 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+package me.henri.multithreading;
+
+public class Main {
+
+    //Method to shorten System.out.println(); method
+    private void println(Object output){
+        System.out.println(output);
+    }
+
+    //Start
+    public static void main(String[] args) {
+
+        //Instantiate main class
+        Main main = new Main();
+
+        //Check for correct syntax
+        if(args.length != 1){
+            main.println("Cannot resolve syntax...");
+            System.exit(0);
+        }
+        //If syntax is correct run test() method on a new thread for every int threads
+        else{
+            int threads = Integer.parseInt(args[0]);
+
+            for(int i = 0; i < threads; i++){
+                new Thread(main.test()).start();
+            }
+        }
+    }
+
+    //Method to run
+    private Runnable test(){
+        return new Runnable() {
+            @Override
+            public void run() {
+                Main main = new Main();
+                main.println("Working");
+            }
+        };
+    }
+}
